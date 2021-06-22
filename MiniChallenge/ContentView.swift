d396fdf85665da7ac6567af440dd85a8c3b44c15
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-06-22 20:31:37 -0300

    Initial Commit

---
+++ MiniChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniChallenge
+//
+//  Created by Djenifer Renata Pereira on 22/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniChallenge/MiniChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallengeApp.swift
+//  MiniChallenge
+//
+//  Created by Djenifer Renata Pereira on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
