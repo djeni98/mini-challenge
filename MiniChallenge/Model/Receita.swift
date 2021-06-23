2bf38a1e705153f3055c0aed4f11690cc30c8569
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-06-23 09:05:47 -0300

    Começa modelo para o projeto

---
+++ MiniChallenge/Model/Receita.swift
@@ -0,0 +1,55 @@
+//
+//  Receita.swift
+//  MiniChallenge
+//
+//  Created by Djenifer Renata Pereira on 22/06/21.
+//
+
+import Foundation
+
+struct Ingrediente {
+    var quantidade: Int?;
+    var nome: String;
+
+    var descricao: String;
+}
+
+enum TipoDeRefeicao {
+    case cafeDaManha;
+    case refeicaoPrincipal; // almoço e janta
+    case lanche;
+}
+
+enum Dificuldade {
+    case iniciante, intermediario, avancado
+}
+
+struct Receita {
+    var categoria: TipoDeRefeicao;
+    var nivelDificuldade: Dificuldade;
+    var nome: String;
+    var tempoPreparo: String;
+    var ingredientes: [Ingrediente];
+    var modoPreparo: String;
+}
+
+enum DiaDaSemana {
+    case domingo, segunda, terca, quarta, quinta, sexta, sabado
+}
+
+
+struct ControleQuantidade {
+    var quantidadeSemana: Int;
+    var quantidadePessoas: Int;
+    var receita: Receita;
+}
+
+struct CardapioDia {
+    var diaDaSemana: DiaDaSemana;
+    var cafeDaManha: ControleQuantidade;
+    var almoco: ControleQuantidade;
+    var jantar: ControleQuantidade;
+    var lanches: [Receita];
+}
+
+
