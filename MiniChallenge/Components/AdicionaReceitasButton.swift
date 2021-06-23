c0007f42afdb6f9abce184caeea6bc828b52cfe7
eros <eros.maurilio@gmail.com>
2021-06-23 11:14:45 -0300

    Cria MainCard

--- MiniChallenge/Components/AdicionaReceitasButton.swift
+++ MiniChallenge/Components/AdicionaReceitasButton.swift
@@ -29,3 +29,4 @@ struct AdicionaReceitasButton_Previews: PreviewProvider {
         AdicionaReceitasButton { print("Run Action") }
     }
 }
+m
---
+++ MiniChallenge/Components/MainCard.swift
@@ -0,0 +1,20 @@
+//
+//  MainCard.swift
+//  MiniChallenge
+//
+//  Created by Eros Maurilio on 23/06/21.
+//
+
+import SwiftUI
+
+struct MainCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MainCard()
+    }
+}
